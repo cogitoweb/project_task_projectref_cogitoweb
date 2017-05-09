0831aaabc58c231ae1372b3b3b65d0316c34ff6a
2z <paolo.cazzitti@cogitoweb.it>
2017-05-09 16:27:10 +0200

    test form

--- model/account_analytic_line.py
+++ model/account_analytic_line.py
@@ -4,3 +4,11 @@ class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
     
     accrual_ids = fields.One2many('account.analytic.line.accrual', 'line_id')
+    
+    @api.model
+    def _check_inv(self, vals):
+    
+        if(not vals.has_key('amount') and not vals.has_key('account_id') and not vals.has_key('journal_id') and not vals.has_key('date') and not vals.has_key('invoice_id') and not vals.has_key('unit_amount') and not vals.has_key('general_account_id')):
+            return True
+
+        return super(AccountAnalyticLine,self)._check_inv(vals)
