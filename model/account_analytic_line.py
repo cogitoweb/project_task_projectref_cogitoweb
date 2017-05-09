21437176bdde1de509831aab920b6fad2880b0a5
2z <paolo.cazzitti@cogitoweb.it>
2017-05-09 16:40:56 +0200

    test form

--- model/account_analytic_line.py
+++ model/account_analytic_line.py
@@ -1,14 +1,19 @@
-from openerp import fields, models, api
+from openerp import fields, models, api, _
+from openerp.exceptions import ValidationError
 
 class AccountAnalyticLine(models.Model):
     _inherit = 'account.analytic.line'
     
     accrual_ids = fields.One2many('account.analytic.line.accrual', 'line_id')
     
-    @api.model
+    @api.multi
     def _check_inv(self, vals):
     
         if('amount' not in vals and 'account_id' not in vals and 'journal_id' not in vals and 'date' not in vals and 'invoice_id' not in vals and 'unit_amount' not in vals and 'general_account_id' not in vals):
             return True
-
-        return super(AccountAnalyticLine,self)._check_inv(vals)
+        else:
+            for r in self:
+                if r.invoice_id:
+                    raise ValidationError(_('You cannot modify an invoiced analytic line!'))
+        
+        return True
